3e83086990bea0841f18e5a6ebedad08cbee8046
AsfaaKhan <khanzadiasfi0314@gmail.com>
2025-05-18 11:23:24 +0500

    Diamond Salon Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Script from "next/script";
 import Headline from "@/components/headline";
+import { SessionProvider } from "next-auth/react";
 
 
 export const metadata: Metadata = {
@@ -20,11 +21,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Headline/>
-        <Header/>
+        <Headline />
+        <SessionProvider>
+          <Header />
+        </SessionProvider>
+
         {children}
-       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" />
-        <Footer/>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" />
+        <Footer />
       </body>
     </html>
   );
